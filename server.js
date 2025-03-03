242a56058437ae8b9a04de6f3d88d0778f437bee
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-03 17:45:35 -0400

    create: serve.js

---
+++ server.js
@@ -0,0 +1,9 @@
+import app from './src/app';
+
+const port = 3003;
+
+app.listen(port, () => {
+  console.log();
+  console.log(`Escutando na porta ${port}`);
+  console.log(`CTRL + CLIQUE em http://localhost: ${port}`);
+});
