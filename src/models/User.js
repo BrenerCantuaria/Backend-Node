40542ddd801a64899c38cac8464544b81c33a1e5
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-04 15:03:49 -0400

    create: Criando model e validações

---
+++ src/models/User.js
@@ -0,0 +1,52 @@
+import { Sequelize, Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+export default class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        nome: {
+          type: Sequelize.DataTypes.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [5, 255],
+              msg: 'Campo ter no mínimo 5 e no máximo 255 caracteres',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.DataTypes.STRING,
+          defaultValue: '',
+          validate: {
+            isEmail: {
+              args: [5, 255],
+              msg: 'Email inválido',
+            },
+          },
+        },
+        passwordHash: {
+          type: Sequelize.DataTypes.STRING,
+          defaultValue: '',
+        },
+        password: {
+          type: Sequelize.DataTypes.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [6, 50],
+              msg: 'Campo ter no mínimo 6 e no máximo 50 caracteres',
+            },
+          },
+        },
+      },
+      {
+        sequelize,
+        tableName: 'users', // Garante que a tabela correta seja usada
+      }
+    );
+    this.addHook('beforeSave', async (user) => {
+      user.passwordHash = await bcryptjs.hash(user.password, 12);
+    });
+    return this;
+  }
+}
