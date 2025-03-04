b107c7c88d3596693fa3ade9e3b9dbd44f3a991e
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-04 14:19:51 -0400

    refactor: to lower case 'User'

--- src/database/migrations/20250304181807-users.js
+++ src/database/migrations/20250304181807-users.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -17,6 +17,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   },
 };
