c4fdc3ef923bc540502ca7306c4c13089bcf6711
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-04 15:05:12 -0400

    add (User): adiciando model User no array

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import { Sequelize } from 'sequelize';
 import databaseConfig from '../config/database.js';
 import Aluno from '../models/Aluno';
+import User from '../models/User.js';
 
 // os models devem ser adicionados no array
-const models = [Aluno];
+const models = [Aluno, User];
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => {
