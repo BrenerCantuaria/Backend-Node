398ee43829e1776dbf0f1b4ad679b801394d1cb5
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-04 20:36:55 -0400

    feat: rotas  de CRUD

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ import UserController from '../controllers/UserController';
 
 const userRouter = new Router();
 
+userRouter.get('/', UserController.index);
 userRouter.post('/create', UserController.create);
-
+userRouter.get('/:id', UserController.show);
+userRouter.put('/:id', UserController.update);
 export default userRouter;
