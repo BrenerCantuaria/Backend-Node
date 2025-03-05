8df76b09e32ef8d7344da9563a4421c71da63fd2
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-04 22:55:37 -0400

    feat: add middleware loginRequired na rota index

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
 
 const userRouter = new Router();
-
-userRouter.get('/', UserController.index);
+// a ordem de importação de middlewares é importante
+userRouter.get('/', loginRequired, UserController.index);
 userRouter.post('/create', UserController.create);
 userRouter.get('/:id', UserController.show);
 userRouter.put('/:id', UserController.update);
