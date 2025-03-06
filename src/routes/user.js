b2bb1694aee5544f8ee956e8f4af1f1984e801ab
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-06 12:28:33 -0400

    refactor: comentando rotas de findAll

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,8 +6,8 @@ const userRouter = new Router();
 // a ordem de importação de middlewares é importante
 // Não deve ir para o sistema final
 // o usuário deve acessar apenas seus proprios dados
-userRouter.get('/', UserController.index);
-userRouter.get('/:id', UserController.show);
+// userRouter.get('/', UserController.index);
+// userRouter.get('/:id', UserController.show);
 
 userRouter.post('/create', UserController.create);
 userRouter.put('/', loginRequired, UserController.update);
