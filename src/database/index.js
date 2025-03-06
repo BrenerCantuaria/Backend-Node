1618ee0ef0e883d32bc6fb6248783e6ec7688007
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-06 12:31:15 -0400

    feat: add model turmas

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,10 @@ import { Sequelize } from 'sequelize';
 import databaseConfig from '../config/database.js';
 import Aluno from '../models/Aluno';
 import User from '../models/User.js';
+import Turma from '../models/Turma.js';
 
 // os models devem ser adicionados no array
-const models = [Aluno, User];
+const models = [Aluno, User, Turma];
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => {
