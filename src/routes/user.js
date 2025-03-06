39fc85af3424b318c20303749f622cdf8ad84d28
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-05 21:53:27 -0400

    refactor: funalidades apenas para testes

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,10 +4,13 @@ import loginRequired from '../middlewares/loginRequired';
 
 const userRouter = new Router();
 // a ordem de importação de middlewares é importante
-userRouter.get('/', loginRequired, UserController.index);
-userRouter.post('/create', UserController.create);
+// Não deve ir para o sistema final
+// o usuário deve acessar apenas seus proprios dados
+userRouter.get('/', UserController.index);
 userRouter.get('/:id', UserController.show);
-userRouter.put('/:id', UserController.update);
-userRouter.delete('/:id', UserController.delete);
+
+userRouter.post('/create', UserController.create);
+userRouter.put('/', loginRequired, UserController.update);
+userRouter.delete('/', loginRequired, UserController.delete);
 
 export default userRouter;
